5065fabdf208c815b2905f763d3b8ba65f8a3e84
hyeoktae kwon <kht_0218@hanmail.net>
2021-01-29 00:57:06 +0900

    feat: tass - operator finish

--- Mastering-RxSwift-master/main/Operators/Transforming Operators/groupBy.playground/Contents.swift
+++ Mastering-RxSwift-master/main/Operators/Transforming Operators/groupBy.playground/Contents.swift
@@ -30,4 +30,46 @@ import RxSwift
 let disposeBag = DisposeBag()
 let words = ["Apple", "Banana", "Orange", "Book", "City", "Axe"]
 
+// 옵져버블이 방출하는 요소를 원하는기준으로 그룹핑할때 사용한다.
+// 파라미터로 클로져를 받고, 클로져는 요소를 파라미터로 받아서 키를 리턴한다.
+// 키의 형식은 Hashable 프로토콜을 채용한 형식으로 한정되어있다.
+// 연산자를 실행하면 클로져에서 동일한 값을 리턴하는 요소끼리 그룹으로 묶이고
+// 그룹에 속한 요소들은 개별 옵져버블을 통해 방출된다.
+// 연산자가 리턴하는 옵져버블은 타입파라미터가 GroupedObservable로 선언되있다.
+// 여기에는 방출하는 요소와 함께 키가 저장되어 있다.
 
+// 문자열 기준으로 그룹핑 해본다.
+
+Observable.from(words)
+  .groupBy { $0.count } // 이러면 키 형태가 Int가 된다. 문자열 길이에 따라 그룹핑
+  .subscribe(onNext: { groupedObservable in
+    print("== \(groupedObservable.key)")
+    groupedObservable.subscribe { print(" \($0)") }
+      .disposed(by: disposeBag)
+  })
+  .disposed(by: disposeBag)
+
+// 그룹바이 연산자를 사용할때는 보통 flatMap와 toArray연산자를 활용해서
+// 그룹핑된 최종결과를 하나의 배열로 방출하도록 구현한다.
+
+Observable.from(words)
+  .groupBy { $0.count }
+  .flatMap { $0.toArray() }
+  .subscribe { print($0) }
+  .disposed(by: disposeBag)
+
+
+// 이번에는 첫번째 문자를 기준으로 그룹핑 해본다.
+
+Observable.from(words)
+  .groupBy { $0.first ?? Character(" ") }
+  .flatMap { $0.toArray() }
+  .subscribe { print($0) }
+  .disposed(by: disposeBag)
+
+// 홀수과 짝수로 나눠보기 도전과제
+Observable.range(start: 1, count: 10)
+  .groupBy { $0 % 2 }
+  .flatMap { $0.toArray() }
+  .subscribe { print($0) }
+  .disposed(by: disposeBag)
--- Mastering-RxSwift-master/main/Operators/Transforming Operators/window.playground/Contents.swift
+++ Mastering-RxSwift-master/main/Operators/Transforming Operators/window.playground/Contents.swift
@@ -29,8 +29,51 @@ import RxSwift
 
 let disposeBag = DisposeBag()
 
+// window: buffer 연산자처럼 타임스팬과 맥스카운트를 지정해서 원본옵져버블이 방출하는 항목들을 작은단위의 옵져버블로 분해한다.
+// 버퍼연산자는 수집된 항목을 배열형태로 리턴하지만 윈도우는 수집된 항목을 방출하는 옵져버블을 리턴한다.
+// 그래서 리턴된 옵져버블이 뭘 방출하고 언제 완료되는지 이해하는것이 중요하다.
+
+Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+  .window(timeSpan: .seconds(5), count: 3, scheduler: MainScheduler.instance)
+  .take(5)
+  .subscribe {
+    print($0)
+    if let observable = $0.element {
+      observable.subscribe { print(" inner: ", $0) }
+        .disposed(by: disposeBag)
+    }
+  }
+  .disposed(by: disposeBag)
 
 
+// 파람은 버퍼와 비슷. 1. 항목을 분해할 시간단위 2. 분해할 최대 항목수 3. 연산자를 실행할 스케쥴러 전달
+// 리턴형이 버퍼와 다르다. 옵져버블을 방출하는 옵져버블을 리턴한다. 이것을 Inner Observable이라고 한다.
+// 이것은 지정된 최대항목수만큼 방출하거나, 지정된 시간이 경과하면 컴플리티드를 전달하고 종료한다.
+// RxSwift.AddRef -> 특별한 형태의 옵져버블 Inner Observable, 이것을 구독할수있다는걸 이해하는거로 충분
+// 시간의 오차로 결과가 상이할 수 있다.
+// 시간이 차지 않아도 maxCount가 차면 바로 방출한다.
 
+/*
+ next(RxSwift.AddRef<Swift.Int>)
+  inner:  next(0)
+  inner:  completed
+ next(RxSwift.AddRef<Swift.Int>)
+  inner:  next(1)
+  inner:  next(2)
+  inner:  next(3)
+  inner:  completed
+ next(RxSwift.AddRef<Swift.Int>)
+  inner:  next(4)
+  inner:  next(5)
+  inner:  completed
+ next(RxSwift.AddRef<Swift.Int>)
+  inner:  next(6)
+  inner:  next(7)
+  inner:  completed
+ next(RxSwift.AddRef<Swift.Int>)
+ completed
+  inner:  next(8)
+  inner:  next(9)
+  inner:  completed
 
-
+ */
